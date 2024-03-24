bf7e6280be73e94db241d37751db449b1396db2e
chuaK <chuakianpheng@gmail.com>
2024-03-24 14:07:24 +0800

    initial commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('manageuser.manage');
+    }
+
+    public function show()
+    {
+        return view('manageuser.manage');
+    }
+}
